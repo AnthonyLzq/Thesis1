d1b9a2fb51edf4ca03b1957988ce6a22163c35e1
AnthonyLzq <sluzquinosa@uni.pe>
2023-01-19 00:34:17 -0500

    feat: initial project configuration

---
+++ app/root.tsx
@@ -0,0 +1,98 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from '@remix-run/react'
+
+import swiperBundle from 'swiper/swiper-bundle.css'
+import swiperCss from 'swiper/swiper.min.css'
+
+import Nunito300 from '@fontsource/nunito/300.css'
+import Nunito400 from '@fontsource/nunito/400.css'
+import Nunito500 from '@fontsource/nunito/500.css'
+import Nunito600 from '@fontsource/nunito/600.css'
+import Nunito700 from '@fontsource/nunito/700.css'
+import NunitoSans300 from '@fontsource/nunito-sans/300.css'
+import NunitoSans400 from '@fontsource/nunito-sans/400.css'
+import NunitoSans600 from '@fontsource/nunito-sans/600.css'
+import NunitoSans700 from '@fontsource/nunito-sans/700.css'
+
+import globalStyles from './styles/global.css'
+
+export const links: LinksFunction = () => [
+  {
+    rel: 'stylesheet',
+    href: swiperBundle
+  },
+  {
+    rel: 'stylesheet',
+    href: swiperCss
+  },
+  {
+    rel: 'stylesheet',
+    href: globalStyles
+  },
+  {
+    rel: 'stylesheet',
+    href: Nunito300
+  },
+  {
+    rel: 'stylesheet',
+    href: Nunito400
+  },
+  {
+    rel: 'stylesheet',
+    href: Nunito500
+  },
+  {
+    rel: 'stylesheet',
+    href: Nunito600
+  },
+  {
+    rel: 'stylesheet',
+    href: Nunito700
+  },
+  {
+    rel: 'stylesheet',
+    href: NunitoSans300
+  },
+  {
+    rel: 'stylesheet',
+    href: NunitoSans400
+  },
+  {
+    rel: 'stylesheet',
+    href: NunitoSans600
+  },
+  {
+    rel: 'stylesheet',
+    href: NunitoSans700
+  }
+]
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'Tesis 1',
+  viewport: 'width=device-width,initial-scale=1'
+})
+
+export default function App() {
+  return (
+    <html lang='en'>
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
---
+++ remix.config.js
@@ -0,0 +1,21 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  serverBuildTarget: 'vercel',
+  serverDependenciesToBundle: [
+    'swiper',
+    'swiper/react',
+    'swiper/react/swiper-react.js',
+    'ssr-window',
+    'ssr-window/ssr-window.esm.js',
+    'dom7'
+  ],
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  ignoredRouteFiles: ['**/.*']
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "api/index.js",
+  // publicPath: "/build/",
+}
