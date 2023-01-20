997d0917166e772acd7b02992abb6dcecbc186f5
AnthonyLzq <sluzquinosa@uni.pe>
2023-01-20 01:27:09 -0500

    feat: improved title

--- app/root.tsx
+++ app/root.tsx
@@ -42,7 +42,7 @@ export const links: LinksFunction = () => [
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'Tesis 1',
+  title: 'Tesis 1 - Anthony Luzquiños',
   viewport: 'width=device-width,initial-scale=1'
 })
 
